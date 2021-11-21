01a0cc5b42de1cb347aeda8b48c5aa315a99ee9b
yousef-shaban <joe.kapo123@gmail.com>
2021-11-21 19:52:41 +0200

    finish the porject

--- js/app.js
+++ js/app.js
@@ -18,45 +18,139 @@
  * Great to have comments before crucial code sections within the procedure.
 */
 
-/**
- * Define Global Variables
- * 
-*/
+// build the navbar
+
+let sections = Array.from(document.getElementsByTagName("section"));
+
+    // get the navbar Element
+let nav_bar = document.getElementById("navbar__list");
+
+    // loop over the sections and append the li to the navbar
+const fragment = document.createDocumentFragment()
+for (let section of sections)
+{
+    let nav_item = document.createElement('li');
+    let anchor = document.createElement('a');
+    anchor.textContent = section.getAttribute("data-nav");
+    anchor.setAttribute('href', '#');
 
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+    nav_item.appendChild(anchor)
+    fragment.appendChild(nav_item);
+}
+    // append the fragment to the navbar element
+nav_bar.appendChild(fragment)
+
+// finish building the navbar
 
 
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
 
-// build the nav
+// click to scroll
+
+let links = [...nav_bar.getElementsByTagName("a")];
+
+function scrollToSection (ev)
+{
+    ev.preventDefault()
+    let link = ev.target;
+    let linkContent = link.textContent;
+    
+    let sections_links_attr = function(sections)
+    {
+        sections.forEach(section => {
+            let data_atr = section.getAttribute('data-nav')
+            if(data_atr === linkContent)
+            {
+                section.scrollIntoView({behavior: "smooth", block: "end", inline: "nearest"})
+                section.setAttribute('class', 'your-active-class')
+                link.setAttribute('class', 'active_link');
+
+                // remove the "active_link" class from the <a>s exept the one that clicked
+                    for(let i = 0; i < links.length; i++)
+                    {
+                        if(links[i] === link)
+                        {
+                            continue;
+                        }
+                        else{
+                            links[i].removeAttribute('class')
+                        }
+                    }
+
+                // remove the "your-active-section"  class from the sections exept the one that is in view
+
+                    for(let i = 0; i < sections.length; i++)
+                    {
+                        if(sections[i].getAttribute('data-nav') === linkContent)
+                        {
+                            continue;
+                        }
+                        else{
+                            sections[i].removeAttribute('class')
+                        }
+                    }
+            }
+        })
+    }
+    sections_links_attr(sections);
+
+}
+links.forEach(link => {
+
+    link.addEventListener("click", scrollToSection)
+})
+
+// finish click to scroll function
 
 
-// Add class 'active' to section when near top of viewport
+
+// is the section in view port function
+
+let isInViewPort = function(ev)
+{
+    let currentSection = ev.target;
+
+    sections.forEach(
+        section => {
+            // get the section coordinates
+            const coor = section.getBoundingClientRect()
+            // check if the element is in the view
+            if(coor.top <= (window.innerHeight * 0.5) && coor.top >= 0)
+            {
+                // add the class your-active-class to the section that is in the view
+                section.classList.add('your-active-class')
+                // add the active-link to the link coresponding to the section in view
+                links.forEach(
+                    link => {
+                        if(section.getAttribute('data-nav') === link.textContent)
+                        {
+                            link.classList.add('active_link')
+                        }
+                        else{
+                            link.classList.remove('active_link')
+
+                        }
+                    }
+                )
+
+            }
+            else{
+                section.classList.remove('your-active-class')
+            }
+        }
+    )
+}
+// add event listener to the document to listen to the scrool event
+document.addEventListener('scroll', isInViewPort)
+
+// finish the section in view port function
 
 
-// Scroll to anchor ID using scrollTO event
+// click menu in phone screen
+let icon = document.getElementById("click_icon")
+let ul = document.getElementById("navbar__list");
 
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+icon.addEventListener("click", ()=> {
+    ul.classList.toggle('visible')
+})
